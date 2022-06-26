f992603a9661369815bc5ea11b3bfa506942289a
dragon731012 <105754685+dragon731012@users.noreply.github.com>
2022-06-26 12:59:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ export default function App() {
 
   return (
     <form className="App" onSubmit={handleSubmit}>
-      <h2>
+      
+  <h2>
         place an order here, and we will deliver with free shipping! Make sure
         you put your adress and what you want, or we won't know where  to deliver.
       </h2>
